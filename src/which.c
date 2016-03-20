6cbbd9a8606ead22e59648026cc5e8b1745ad8ef
MrLYC <imyikong@gmail.com>
2016-03-20 18:22:18 +0800

    add which command line parser

---
+++ src/which.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+const int SUCCESSNO = 0;
+const int SYSERRNO = -1;
+const int USRERRNO = 1;
+const int OPTFINISHED = -1;
+
+
+typedef struct {
+    bool option_all;
+    bool option_help;
+    char * filename;
+} Argument;
+
+
+bool parse_command_line(Argument * arg_ptr, int argc, char * const argv[]) {
+    int option;
+
+    arg_ptr->option_all = false;
+    arg_ptr->option_help = false;
+    arg_ptr->filename = NULL;
+
+    while ((option = getopt(argc, argv, "ah")) != OPTFINISHED) {
+        switch (option) {
+            case 'a':
+                arg_ptr->option_all = true;
+                break;
+            case 'h':
+                arg_ptr->option_help = true;
+                break;
+            default:
+                return false;
+        }
+    }
+
+    if (optind >= argc) {
+        return false;
+    }
+
+    arg_ptr->filename = argv[optind];
+
+    if (strlen(arg_ptr->filename) == 0) {
+        return false;
+    }
+
+    return true;
+}
+
+
+void print_usage() {
+    printf("which - locate a command\n");
+
+    printf("\npositional arguments:\n");
+    printf("\tfilename\n");
+
+    printf("\noptional arguments:\n");
+    printf("\t-h  show this help message and exit\n");
+    printf("\t-a  print all matching pathnames of each argument\n");
+}
+
+
+int main(int argc, char * const argv[]) {
+    Argument arg = {false, false, NULL};
+    if (!parse_command_line(&arg, argc, argv)) {
+        print_usage();
+        return USRERRNO;
+    }
+    else if (arg.option_help) {
+        print_usage();
+        return SUCCESSNO;
+    }
+
+    // TODO: find the command
+
+    return SUCCESSNO;
+}
